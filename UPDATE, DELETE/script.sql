5df590c323991cf3f47f3b584701f440703a43e4
chaimae12444 <sedraoui.chaimae.solicode@gmail.com>
2026-01-06 10:38:58 +0100

    Add files via upload

---
+++ UPDATE, DELETE/script.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE indix;
+USE indix;
+CREATE TABLE utilisateur (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(25)NOT NULL,
+    email VARCHAR(100)NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(200)
+);
+INSERT INTO utilisateur (nom, email, mot_de_passe) VALUES
+('Alice', 'alice@test.com', '1234'),
+('Bob', 'bob@test.com', 'passbob'),
+('Charlie', 'charlie@test.com', 'passcharlie');
+UPDATE utilisateur 
+SET nom = 'Alicename', email= 'alicename@test.com'
+WHERE id = 1;
+DELETE FROM utilisateur
+WHERE id = 2;
+SELECT * FROM Utilisateur;
